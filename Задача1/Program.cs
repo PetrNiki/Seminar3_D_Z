57b93105f76f54d252d5fb6a93d2a270caba374c
PetrNiki <nikitenkopv@yandex.ru>
2022-07-17 09:50:24 +0300

    Задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+//число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите число");
+string namber  = Console.ReadLine();
+if (namber[0] == namber[4] || namber[1] == namber[3])
+{
+    Console.WriteLine("Это число палиндром");
+}
+else
+{
+   Console.WriteLine("Это число не палиндром"); 
+}
